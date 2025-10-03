31ca93f1ad3270827272777cdb4605b04bb09e93
Louis Gautier <Louis.gautier@outlook.fr>
2025-10-03 20:15:41 +0800

    Use env var for compilerOpts

--- client/domain/notification/build.gradle.kts
+++ client/domain/notification/build.gradle.kts
@@ -1,17 +1,31 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.convention.plugin)
 }
 
 kotlin {
+    val localProperties = Properties().apply {
+        load(FileInputStream(File(rootProject.rootDir, "firebase.properties")))
+    }
+    val firebaseSdkPath: String = localProperties.getProperty("FIREBASE_SDK_PATH").toString()
 
     listOf(
         iosX64(),
         iosArm64(),
         iosSimulatorArm64()
     ).forEach {
+
         it.compilations["main"].cinterops.create("firebase") {
             definitionFile = file("src/nativeInterop/cinterop/firebase.def")
+            compilerOpts(
+                "-I${firebaseSdkPath}/FirebaseRemoteConfig/Sources/Public",
+                "-I${firebaseSdkPath}/FirebaseRemoteConfig/Sources/Public/FirebaseRemoteConfig",
+                "-I${firebaseSdkPath}/FirebaseCore/Sources/Public",
+                "-I${firebaseSdkPath}/FirebaseCore/Sources/Public/FirebaseCore"
+            )
         }
     }
 
@@ -27,4 +41,22 @@ kotlin {
             implementation(libs.kotlinx.coroutines.play.services)
         }
     }
+
+//    targets.withType<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget> {
+//        compilations["main"].cinterops {
+//            val firebase by creating {
+//                definitionFile.set(project.file("src/nativeInterop/cinterop/firebase.def"))
+//
+//                val firebaseSdkPath: String = project.findProperty("FIREBASE_SDK_PATH").toString()
+//                    //?: error("FIREBASE_SDK_PATH is not defined in gradle.properties")
+//
+//                compilerOpts(
+//                    "-I${firebaseSdkPath}/FirebaseRemoteConfig/Sources/Public",
+//                    "-I${firebaseSdkPath}/FirebaseRemoteConfig/Sources/Public/FirebaseRemoteConfig",
+//                    "-I${firebaseSdkPath}/FirebaseCore/Sources/Public",
+//                    "-I${firebaseSdkPath}/FirebaseCore/Sources/Public/FirebaseCore"
+//                )
+//            }
+//        }
+//    }
 }
